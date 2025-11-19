c568f9636b8954aca3e07fc87110f98c60a7499a
rozievich <oybekrozievich@gmail.com>
2025-11-19 16:43:40 +0500

    Fixed bugs and optimizing code

--- config/urls.py
+++ config/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path, include
+from django.contrib import admin
 
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
@@ -22,5 +23,6 @@ schema_view = get_schema_view(
 urlpatterns = [
    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
    path('swdoc/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+   path('panel/', admin.site.urls),
    path("api/v1/", include("letters.urls"))
 ]
--- letters/admin.py
+++ letters/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from .models import Letter
 # Register your models here.
+
+admin.site.register(Letter)
